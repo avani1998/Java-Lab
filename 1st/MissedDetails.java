502d02a131a83cbc1319bb7378d3b120162816f2
avani1998 <46353367+avani1998@users.noreply.github.com>
2020-03-12 00:06:45 +0530

    Create MissedDetails.java

---
+++ 1st/MissedDetails.java
@@ -0,0 +1,15 @@
+
+public class MissedDetails {
+String name,time;
+long num;
+MissedDetails(String n,String t,long nu)
+{
+	name="private Caller";
+	time=t;
+	num=nu;
+}
+public String toString()
+{
+	return "++";
+}
+}
