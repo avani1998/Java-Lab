2a24a4f278b4f740f4cf9c74a774ffaef5c91636
avani1998 <46353367+avani1998@users.noreply.github.com>
2020-03-11 21:54:32 +0530

    Main Class

---
+++ 3rd/Updated/sgpaMain.java
@@ -0,0 +1,12 @@
+import java.awt.GridLayout;
+import java.awt.*;
+public class sgpaMain 
+{
+	public static void main(String args[])
+	{
+		sgpaGui obj=new sgpaGui();
+		obj.f1.setVisible(true);
+		obj.f1.setSize(800, 800);
+		obj.f1.setLayout(new FlowLayout());
+}
+}
