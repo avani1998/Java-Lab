f3b7357fc82bf3c9bcdcc6d77739aebd8b7613ea
avani1998 <46353367+avani1998@users.noreply.github.com>
2020-03-11 21:51:46 +0530

    Create Book.java

---
+++ 2nd/Book.java
@@ -0,0 +1,17 @@
+public class Book 
+{
+String title,publication,author;
+int price;
+Book(String tit,String pub,String au,int pr)
+{
+	title=tit;
+	publication=pub;
+	author=au;
+	price=pr;
+	}
+public String toString()
+{
+	return title;
+}
+
+}
